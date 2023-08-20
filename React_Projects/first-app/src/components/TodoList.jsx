4a850eecc3de742667548552698a796515e1d3ee
shahajahan feroz <shahajahanferoz@gmail.com>
2023-08-20 14:32:02 +0600

    TODO List Done

--- React_Projects/first-app/src/components/TodoList.jsx
+++ React_Projects/first-app/src/components/TodoList.jsx
@@ -1,51 +1,86 @@
-import React, {useState} from 'react'
+import React, { useState, useRef } from "react";
 
 const TodoList = () => {
-    const [inputTask, setInputTask] = useState('');
-    const [list, setList] = useState([]);
+  const [inputTask, setInputTask] = useState("");
+  const [list, setList] = useState([]);
+  const [editId, setEditId] = useState(0);
+  const focusInput = useRef(null);
 
-    const handleAddTodo = () => {
+  const handleAddTodo = () => {
+    if(!editId){
         const newTask = {
             id: Math.random(),
-            todo: inputTask
-        };
+            todo: inputTask,
+          };
+      
+          // console.log(inputTask);
+          setList([...list, newTask]);
+    }else{
+        const editableTask = list.find((todo) => todo.id === editId);
+    editableTask.todo = inputTask;
+        setEditId(0)
+    }
+    setInputTask("");
+  };
 
-       setList([...list, newTask]);
-        setInputTask('');
-    };
+  const handleEditTodo = (id) => {
+    const editableTask = list.find((todo) => todo.id === id);
+    setInputTask(editableTask.todo);
+    focusInput.current.focus();
+    setEditId(id);
+  };
 
-   const handleDeleteTodo = (id) => {
-        const newList = list.filter((todo) => todo.id !== id);
-        setList(newList);
-    };
+  const UpdateList = () => {
+    const editableTask = list.find((todo) => todo.id === editId);
+    editableTask.todo = inputTask;
+    setInputTask("");
+  };
 
-   const handleInputChange = (event) => {
-        setInputTask(event.target.value);
-    };
+  const handleDeleteTodo = (id) => {
+    const newList = list.filter((todo) => todo.id !== id);
+    setList(newList);
+  };
 
-   return (
+  const handleInputChange = (event) => {
+    setInputTask(event.target.value);
+  };
+  return (
     <div className="Todo">
-        <h1>My To-Do List</h1>
+      <h1>My To-Do List</h1>
 
-        <div className="Top">
-            <input className="input" type="text" value={inputTask}
-               onChange={handleInputChange} placeholder="Enter a task" />
+      <div className="Top">
+        <input
+          id="inputFiels"
+          className="input"
+          ref={focusInput}
+          type="text"
+          value={inputTask}
+          onChange={handleInputChange}
+          placeholder="Enter a task"
+        />
 
-            <button className="btn" onClick={handleAddTodo}>ADD</button>
-        </div>
+        <button
+          className="btn"
+          disabled={inputTask.trim() === ""}
+          onClick={handleAddTodo}
+        >
+          {editId?"Edit":"ADD"}
+        </button>
+      </div>
 
-       <ul>
-            { list.map((todo) => (
-                <li className="task" key={todo.id}>
-                    {todo.todo}
-                    <button onClick={() => handleDeleteTodo(todo.id)}>
-                       Delete
-                   </button>
-                </li>
-            ))}
-        </ul>
+      <ul>
+        {list.map((todo) => (
+          <li className="task" key={todo.id}>
+            <div className="listShow">{todo.todo}</div>
+            <div>
+              <button onClick={() => handleEditTodo(todo.id)}>Edit</button>
+              <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
-);
+  );
 };
 
 export default TodoList;
